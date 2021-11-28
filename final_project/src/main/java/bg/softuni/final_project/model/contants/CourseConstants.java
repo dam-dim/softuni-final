456dfaa88ef284c775a97f7ad3400c738d9c7782
Damyan Dimov <ddimov99@mail.bg>
2021-11-28 14:21:52 +0200

    Constants And Validator

---
+++ final_project/src/main/java/bg/softuni/final_project/model/contants/CourseConstants.java
@@ -0,0 +1,33 @@
+package bg.softuni.final_project.model.contants;
+
+import bg.softuni.final_project.model.entity.enums.CourseLevelEnum;
+
+import java.util.List;
+
+import static bg.softuni.final_project.model.entity.enums.CourseLevelEnum.*;
+
+public class CourseConstants {
+    public static final List<String> INIT_COURSE_NAMES = List.of(
+            "Open Water Diver",
+            "Advanced Open Water Diver",
+            "Divemaster",
+            "Instructor",
+            "Trimix"
+    );
+
+    public static final List<CourseLevelEnum> INIT_COURSE_LEVELS = List.of(
+            BEGINNER,
+            ADVANCED,
+            PROFESSIONAL,
+            PROFESSIONAL,
+            TECH
+    );
+
+    public static final List<String> INIT_COURSE_DESCRIPTIONS = List.of(
+            "Open Water Diver",
+            "Advanced Open Water Diver",
+            "Divemaster",
+            "Instructor",
+            "Trimix"
+    );
+}
---
+++ final_project/src/main/java/bg/softuni/final_project/model/contants/DiveConstants.java
@@ -0,0 +1,31 @@
+package bg.softuni.final_project.model.contants;
+
+import bg.softuni.final_project.model.entity.enums.DiveLevelEnum;
+
+import java.util.List;
+
+import static bg.softuni.final_project.model.entity.enums.DiveLevelEnum.*;
+import static bg.softuni.final_project.model.entity.enums.DiveLevelEnum.TECH;
+
+public class DiveConstants {
+    public static final List<String> INIT_DIVE_TYPES = List.of(
+            "Try Dive",
+            "Reck Dive",
+            "Night Dive",
+            "Deep Dive"
+    );
+
+    public static final List<DiveLevelEnum> INIT_DIVE_LEVELS = List.of(
+            BEGINNER,
+            ADVANCED,
+            PROFESSIONAL,
+            TECH
+    );
+
+    public static final List<String> INIT_DIVE_DESCRIPTIONS = List.of(
+            "Try Dive",
+            "Reck Dive",
+            "Night Dive",
+            "Deep Dive"
+    );
+}
